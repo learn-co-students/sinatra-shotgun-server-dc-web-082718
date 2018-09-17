bd50ece526b0a7e702bc6021259f00ac7fe90c4f
github username <github email address>
2018-09-17 10:13:04 -0400

    Learning sinatra shotgun with simple hello world.

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Welcome to your app!!!! Get yourself together man! Prayed then I prayed again. Had it moment but it came and went. This shit aint no hun-nant benz!"
   end
 
 end
